7e604ce742d3d57a32be699c436e33e2abeff182
The User <actualuser@Varuns-MacBook-Pro.local>
2019-03-05 22:51:07 -0500

    updated .gitignore

---
+++ src/test/java/dijkstra/DijkstraTest.java
@@ -0,0 +1,12 @@
+package dijkstra;
+
+/**
+ * @author vx5
+ *
+ *         This class tests the Dijkstra object as well as, implicitly, the
+ *         functionality of a class implementing the DijkstraDbOp interface.
+ */
+public class DijkstraTest {
+  // Stores instance of Dijkstra object to be used
+
+}
